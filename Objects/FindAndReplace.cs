41e59bfea845225987c3d5b226995870e825dcb9
Michael and Jay <student@epicodus.com>
2016-02-17 13:33:14 -0800

    add find and replace functionality

--- Objects/FindAndReplace.cs
+++ Objects/FindAndReplace.cs
@@ -6,7 +6,11 @@ namespace FindAndReplaceNS
 {
   public class FindAndReplace
   {
-
+    public string FandR(string sentence, string find, string replace)
+    {
+      string result = sentence.Replace(find, replace);
+      return result;
+    }
 
 
   }
--- Tests/FindAndReplaceTest.cs
+++ Tests/FindAndReplaceTest.cs
@@ -6,6 +6,19 @@ namespace FindAndReplaceNS
 {
   public class FindAndReplaceTest
   {
-
+    [Fact]
+    public void FandR_Sentence()
+    {
+      FindAndReplace test = new FindAndReplace();
+      Assert.Equal("Hello dog", test.FandR("Hello cat", "cat", "dog"));
     }
+    [Fact]
+    public void FandR_Partial()
+    {
+      FindAndReplace test = new FindAndReplace();
+      Console.WriteLine(test.FandR("I am walking my cat to the cathedral", "cat", "dog")); 
+      Assert.Equal("I am walking my dog to the doghedral", test.FandR("I am walking my cat to the cathedral", "cat", "dog"));
+    }
+
+  }
 }
