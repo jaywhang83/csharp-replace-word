c26f22db815f899b1c6b277f6d189d2053622854
Michael and Jay <student@epicodus.com>
2016-02-17 13:10:54 -0800

    initial commit

---
+++ Objects/FindAndReplace.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FindAndReplaceNS
+{
+  public class FindAndReplace
+  {
+
+
+
+  }
+}
---
+++ Tests/FindAndReplaceTest.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FindAndReplaceNS
+{
+  public class FindAndReplaceTest
+  {
+
+    }
+}
